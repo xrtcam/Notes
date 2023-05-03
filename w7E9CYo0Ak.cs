8f7e26d63f8773bc362553298267c7ddb330d78b
XrtCam <97391499+xrtcam@users.noreply.github.com>
2023-05-03 10:21:58 +0300

    Create w7E9CYo0Ak.cs
    С# . Code Notes . to sort.

---
+++ w7E9CYo0Ak.cs
@@ -0,0 +1,38 @@
+//--------------------------------------------------
+class Program
+{
+    static void Main()
+    {
+        Sysytem.Console.WriteLine("Hello");
+    }
+}
+//--------------------------------------------------
+using System;
+                    
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello World");
+    }
+}
+//--------------------------------------------------
+//Rextester.Program.Main is the entry point for your code. Don't change it.
+//Compiler version 4.0.30319.17929 for Microsoft (R) .NET Framework 4.5
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Rextester
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Your code goes here
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
